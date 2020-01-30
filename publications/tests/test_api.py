3633e3b0de755019e108e080fc165c1e9310f217
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2020-01-30 15:58:26 +0300

    Перенёс client в глобальный conftest, переименовал в api_client

--- publications/tests/test_api.py
+++ publications/tests/test_api.py
@@ -1,19 +1,13 @@
 import pytest
-from rest_framework.test import APIClient
-
-
-@pytest.fixture
-def client():
-    return APIClient()
 
 
 @pytest.mark.django_db
-def test_publication_list(client, publication):
-    response = client.get('/api/publications/')
+def test_publication_list(api_client, publication):
+    response = api_client.get('/api/publications/')
     assert response.json()[0]['id'] == 1
 
 
 @pytest.mark.django_db
-def test_publication_retrieve(client, publication):
-    response = client.get('/api/publications/1/')
+def test_publication_retrieve(api_client, publication):
+    response = api_client.get('/api/publications/1/')
     assert response.json()['id'] == 1
